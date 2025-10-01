41e1c0192e0b5b54e01505e165bff1d81c227a48
ssawaul <ssawaul@gmail.com>
2025-10-01 20:06:09 +0900

    1001

--- 자바/src/CircleArea.java
+++ 자바/src/CircleArea.java
@@ -1,10 +1,37 @@
 public class CircleArea {
 // main() 메소드에서 실행 시작
+    int radius; // 반지름
+    String name;
+    public CircleArea() {
+        radius = 1; 
+        name = " ";
+    }
+    public CircleArea(int radius)
+    {
+        this (radius, "원");
+        this.radius = radius;
+        
+    }
+    public CircleArea(int r, String n)
+    {
+        radius = r; 
+        name = n;
+    }
+
+    public double getArea() {
+        final double PI = 3.14; // 원주율 상수
+        return radius * radius * PI; // 원의 넓이
+    }
+
+
 public static void main(String[] args) 
 {
-    final double PI = 3.14; // 원주율 상수
-    int radius = 10;        // 반지름
-    double area = radius * 2 * PI;
-    System.out.println("원의 면적 =" + area);           // 원의 넓이
+    CircleArea [] c = new CircleArea[5];
+    for (int i = 0; i < 5; i++)
+    {
+        c[i] = new CircleArea(i);
+        System.out.println(c[i].name + "의 면적은 " + c[i].getArea());
+    } 
+    
 }
 }
