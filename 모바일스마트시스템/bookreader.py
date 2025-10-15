4959f873acc9a19e14ff73ca6e19ddc0b9a30d17
ssawaul <ssawaul@gmail.com>
2025-10-16 01:49:42 +0900

    234

--- 모바일스마트시스템/bookreader.py
+++ 모바일스마트시스템/bookreader.py
@@ -1,3 +1,7 @@
-file = open("bookwriter.txt", "r")
-for line in file.readlines():
-    print(line, end="")
+file = open("bookwriter.txt", "r") # 읽기 모드로 열기
+
+for line in file.readlines(): # 한 줄씩 읽기
+    data = line.strip().split(",") # , 기준으로 나누어서 data[0], data[1]에 저장
+    print("이름은 %s, 전화번호는 %s " % (data[0], data[1]))
+    print(type(line))
+file.close() # 파일 닫기
